136b407f8770092e41817a0f5de36fedef317661
DioDeng <141546488+DioDeng@users.noreply.github.com>
2025-02-06 15:02:08 +0800

    Update linepays.js

--- controllers/linepays.js
+++ controllers/linepays.js
@@ -77,7 +77,7 @@ const linepays = {
     const linePayRes = await axios.post(url, linePayBody, { headers });
 
     // 重新導向前端連結，讓前端重新戳api得到已付款資訊
-    res.redirect(`http://localhost:8080/#/order/${isOrderExist.cookieId}`);
+    res.redirect(`https://diodeng.github.io/LULU_Bake_Front/#/order/${isOrderExist.cookieId}`);
   },
   // 建立訂單
   async createLinepay(req, res, next) {
